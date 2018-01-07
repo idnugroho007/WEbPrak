d77b3e6a22d92bb2de4df4054f3a17956e706b8e
nugroho <raharjonugroho007@gmail.com>
2018-01-07 22:26:27 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,287 @@
+<html><head lang="en">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="description" content="Untuk memenuhi Tugas Besar Desain Web Praktek.">
+    <meta name="keywords" content="HTML, CSS, JS, JavaScript, framework, metro, front-end, one page">
+    <meta name="author" content="Dewa">
+
+    <link rel="shortcut icon" type="assets/image/x-icon" href="uty.png">
+    <title>Pandawa Travel</title>
+
+    <link href="assets/css/metro.css" rel="stylesheet">
+    <link href="assets/css/metro-icons.css" rel="stylesheet">
+    <link href="assets/css/metro-responsive.css" rel="stylesheet">
+    <link href="assets/css/metro-schemes.css" rel="stylesheet">
+
+    <script src="assets/js/jquery-2.1.3.min.js"></script>
+    <script src="assets/js/metro.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/google/code-prettify/master/loader/prettify.css">
+<body>
+    <div><header class="app-bar fixed-top navy" data-role="appbar">
+    <div class="container">
+        <a href="index.php" class="app-bar-element branding"><span class="mif-home icon"></span> Home</a>
+
+        <ul class="app-bar-menu small-dropdown">
+            <li data-flexorderorigin="0" data-flexorder="2" class="">
+                <a href="#profil"><span class="mif-user icon"></span> Profile</a>
+            </li>
+            <li data-flexorderorigin="1" data-flexorder="2">
+                <a href="#destinasi"><span class="mif-target icon"></span> Destination</a>
+            </li>
+
+            <li data-flexorderorigin="2" data-flexorder="3">
+                <a href="#pemesanan"><span class="mif-cart icon"></span> Reservation</a>
+            </li>
+
+            <li data-flexorderorigin="3" data-flexorder="4">
+                <a href="#kontak"><span class="mif-contacts-dialer icon"></span> Contact</a>
+            </li>
+        </ul>
+
+        <span class="app-bar-pull"></span>
+
+    <div class="app-bar-pullbutton automatic" style="display: none;"></div><div class="clearfix" style="width: 0;"></div><nav class="app-bar-pullmenu hidden flexstyle-app-bar-menu" style="display: none;"><ul class="app-bar-pullmenubar hidden app-bar-menu"></ul></nav></div>
+</header>
+</div><div class="page-content">
+        <div class="bg-lightCyan fg-white align-center">
+            <div class="container">
+                <div class="no-overflow" style="padding-top: 40px">
+
+                    <br>
+                    <br>
+                    <br>
+                    <h1 style="font-size: 4.5rem; line-height: 1" class="text-shadow metro-title text-light">Pandawa Travel</h1>
+                   
+
+                    <div class="grid no-margin-bottom" style="margin-top: 100px" id="g1">
+                        <div class="row cells3">
+                            <div class="cell no-overflow" style="height: 85px">
+                                <div class="bg-gray fg-white block-shadow" style="height: 85px; padding-top: 20px; margin-top: 0px;">
+                                    <h2 class="text-light">Mudah</h2>
+                                </div>
+                            </div>
+                            <div class="cell no-overflow" style="height: 85px">
+                                <div class="bg-lightOlive fg-white block-shadow" style="height: 85px; padding-top: 20px; margin-top: 0px;">
+                                    <h2 class="text-light">Hemat</h2>
+                                </div>
+                            </div>
+                            <div class="cell no-overflow" style="height: 85px">
+                                <div class="bg-DarkEmerald fg-white block-shadow" style="height: 85px; padding-top: 20px; margin-top: 0px;">
+                                    <h2 class="text-light">Cepat</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
+
+        <div class="fg-dark">
+            <div id="profil" class="container">
+                <div class="padding80" style="padding-top: 40px">
+                    <div class="">
+                        <div class="grid">
+                            <div class="row">            		
+                                <div class="cell padding20 clear-float">
+					               <div>
+	                                    <img id="book_logo" src="assets/image/contact-bg.jpg" class="place-left" style="height: 190px; margin-right: 20px;">
+	                                    <h2 class="text-light">Pandawa Travel</h2>
+	                                    <h3 class="fg-red">Sekilas tentang Pandawa</h3>
+	                                    <div class="fg-dark">
+	                                        Pandawa travel adalah perusahaan yang bergerak di bidang pariwisata. Berdiri Tahun 2017, dan telah memiliki cabang di tiga kota di Indonesia seperti Jakarta, Pemalang, Yogyakarta,.
+	                                    </div>
+					               </div>
+	                               </a>
+				                </div>
+                                <div class="cell padding20 clear-float">
+                                   <div>
+                                        <img id="book_logo" src="assets/image/pexels-photo-297642.jpeg" class="place-right" style="height: 190px; margin-right: 20px;">
+                                        <h1 class="">Keunggulan :</h1>
+                                        <ol class="numeric-list square-marker">
+                                            <li>Memiliki sarana dan prasarana yang memadai</li>
+                                            <li>Harga ekonomis</li>
+                                            <li>memiliki staff yang profesional</li>
+                                            <li>melayani setiap keluhan pelanggan</li>
+                                        </ol>
+                                   </div>
+                                   </a>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+    <hr class="thin" ></div>
+    <div class="main-content clear-float" id="destinasi">
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="tile-area no-padding">
+            <div class="tile-group no-margin no-padding" style="width: 100%">
+                <div class="tile-group no-margin no-padding" style="width: 100%">
+                    <div class="tile-large tile-super-y bg-dark ol-transparent" style="float: right; ">
+                        <div class="tile-content">
+                            <div class="brand padding10">
+                                <h3 class="fg-white">Pasang Iklan..</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="tile-large ol-transparent" data-role="tile">
+                        <div class="tile-content">
+                            <div class="carousel" data-role="carousel" data-height="100%" data-width="100%" data-controls="false">
+                                <div class="slide">
+                                    <img src="assets/image/wisata/pexels-photo-238622.jpeg" data-role="fitImage" data-format="fill">
+                                </div>
+                                <div class="slide">
+                                    <img src="assets/image/wisata/pexels-photo-338515.jpeg"  data-role="fitImage" data-format="fill">
+                                </div>
+                                <div class="slide">
+                                    <img src="assets/image/wisata/pexels-photo-672358.jpeg"  data-role="fitImage" data-format="fill">
+                                </div>
+                               
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tile bg-lightBlue fg-white" data-role="tile">
+                        <div class="tile-content iconic">
+                            <span class="icon mif-earth mif-ani-ripple"></span>
+                        </div>
+                    </div>
+                    <div class="tile bg-orange fg-white" data-role="tile">
+                        <div class="tile-content iconic">
+                            <span class="icon mif-motorcycle mif-ani-float"></span>
+                        </div>
+                    </div>
+                    <div class="tile bg-teal fg-white" data-role="tile">
+                        <div class="tile-content iconic">
+                            <span class="icon mif-automobile mif-ani-heartbeat"></span>
+                        </div>
+                    </div>
+                    <div class="tile bg-green fg-white" data-role="tile">
+                        <div class="tile-content iconic">
+                            <span class="icon mif-thumbs-up mif-ani-bounce"></span>
+                        </div>
+                    </div>
+
+                    <div class="tile-big tile-wide-y bg-white" data-role="tile">
+                        <div class="tile-content">
+                            <div class="panel" style="height: 100%">
+                                <div class="heading bg-steel fg-white"><span class="title text-light">Best Destination</span></div>
+                                <div class="content fg-dark clear-float" style="height: 100%">
+                                    <img src="assets/image/wisata/pexels-photo-248797.jpeg" class="place-left margin10" style="height: 120px">
+                                    <h2 class="text-light">Pantai Widuri</h2>
+                                    <p>Pantai yang dikenal dengan pantai Tlincing ini merupakan salah satu objek wisata andalan Kabupaten Pemalang. Pantai Widuri menawarkan pemandangan alam pantai yang indah dan elok. Jika anda datang saat pagi hari atau sore hari, jangan lewatkan untuk menikmati pemandangan sunrise atau sunset yang mempesona.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tile tile-wide-y bg-white" data-role="tile">
+                        <div class="tile-content">
+                            <div class="panel">
+                                <div class="heading bg-violet fg-white"><span class="title text-light">Tips</span></div>
+                                <div class="content bg-lightRed fg-white clear-float">
+                                    <img src="assets/image/pexels-photo-346805.jpeg" class="">
+                                    <strong>Perlengkapan</strong> Pastikan selalu ingat barang bawaan anda agar tidak tertukar dengan yang lain.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> <!-- End first group -->
+
+                
+            </div></div></div></div>
+
+    <div>         
+    <footer style="background-color: #EFEAE3">
+    <div class="bg-lightBlue no-tablet-portrait no-phone">
+        <div class="container padding20 fg-white">
+            <div class="carousel bg-transparent" data-role="carousel" data-controls="false" data-markers="false" data-effect="fade" data-height="600" style="width: 100%; height: 600px;">
+                <div class="slide fg-white">
+                    <!-- pesen -->
+                    <div id="pemesanan" class="container padding40 fg-white">
+                        <h3 class="text-light">Lakukan pemesanan sekarang juga di <strong>Pandawa Travel</strong></h3>  
+                        <form>
+                        <div class="grid">
+                            <div class="row cells2">
+                                <div class="cell">
+                                    <div class="input-control modern text iconic" data-role="input">
+                                        <input type="text" style="padding-right: 5px;">
+                                        <span class="label">Nama</span>
+                                        <span class="informer">Masukkan nama</span>
+                                        <span class="placeholder" style="display: block;">Nama lengkap</span>
+                                        <span class="icon mif-user"></span>
+                                    </div>
+                                </div>
+                                <div class="cell">
+                                    <div class="input-control modern text iconic" data-role="input">
+                                        <input type="email" style="padding-right: 5px;">
+                                        <span class="label">Email</span>
+                                        <span class="informer">Masukkan alamat email</span>
+                                        <span class="placeholder" style="display: block;">Alamat email</span>
+                                        <span class="icon mif-mail"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row cells2">
+                                <div class="cell">
+                                    <div class="input-control modern text iconic" data-role="input">
+                                        <input type="number" style="padding-right: 5px;">
+                                        <span class="label">Nomor HP</span>
+                                        <span class="informer">Masukkan nomor HP</span>
+                                        <span class="placeholder" style="display: block;">Nomor HP</span>
+                                        <span class="icon mif-contacts-dialer"></span>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            <div clear-float>
+                                <label class="input-control checkbox small-check">
+                                <input type="checkbox"">
+                                <span class="check"></span>
+                                <span class="caption">Saya setuju dengan syarat dan aturan yang berlaku!</span>
+                            </label>
+                            </div>
+                            <button action="" class="button big-button block-shadow success loading-pulse lighten" align="align-center"> Pesan</button>
+                        </div>
+                    </form>
+                    </div>
+                    <!-- smpe kene wae -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="kontak" class="container">
+        <!-- kontakkkk-->
+        <div class="cell colspan3 padding20 no-padding-top no-padding-bottom">
+            <h2>Kontak</h2>
+            <p>
+                Informasi lebih lanjut, hubungi:
+            </p>
+            <div>
+                <div class="icon mif-home mif-lg"><h3>Pogung Kidul, Sleman, Yogyakarta</h3></div>
+                <div class="icon mif-contacts-dialer mif-lg"><h3>085678901</h3></span>
+                <div class="icon mif-mail mif-lg"><a href="mailto:raharjonugroho007@gmail.com"><h3>raharjonugroho007@gmail.com</h3></a></div>
+                <div class="icon mif-facebook mif-lg"><a href="https://#"><h3>IDPandawaTravel</h3></a></div>
+                <div class="icon mif-twitter mif-lg"><a href="https://#"><h3>@PandawaTravel</h3></a></div>
+                <div class="icon mif-earth mif-lg"><a href="https://#"><h3>www.travelpandawa.com</h3></a></div>
+            </div>
+        </div>
+
+        <div class="align-center padding20 text-small">
+            Copyright 2017 <a href="index.php">Pandawa Travel</a>
+        </div>
+    </div>
+</footer>
+</div>
+
+</body></html>
